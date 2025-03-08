54fa98f9e5f85f846ca21ecba3ca459009b3ab28
vuxuannhi <tuka.ceo@gmail.com>
2025-03-08 11:16:23 +0700

    mm

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,3 +93,4 @@ if prompt := st.chat_input("Bạn nhập nội dung cần trao đổi ở đây 
     with st.chat_message("assistant"):
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+Tôi đang thử phần Mềm thôi
